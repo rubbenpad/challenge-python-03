2958ebb157a803aa2bb306156bb2b29b3da93ed1
Ruben Padilla <rubbenpad2@gmail.com>
2020-03-30 13:20:21 -0500

    challenge-python-03 solved

--- src/main.py
+++ src/main.py
@@ -1,8 +1,15 @@
-# Resolve the problem!!
+import re
 
 
 def run():
     # Start coding here
+    text = ''
+    with open('./src/encoded.txt', 'r', encoding='utf-8') as f:
+        text = f.read()
+
+    message = ''.join(re.findall(r'\w+(?a)', text))
+
+    return message
 
 
 if __name__ == '__main__':
