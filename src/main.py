5feea0908957d0efa8b79da9e355483be2124163
Ruben Padilla <rubbenpad2@gmail.com>
2020-04-05 17:02:42 -0500

    Fix deprecate warning message

--- src/main.py
+++ src/main.py
@@ -4,10 +4,11 @@ import re
 def run():
     # Start coding here
     text = ''
+    regex = re.compile(r'[a-zA-Z]')
     with open('./src/encoded.txt', 'r', encoding='utf-8') as f:
         text = f.read()
 
-    message = ''.join(re.findall(r'\w+(?a)', text))
+    message = ''.join(re.findall(regex, text))
 
     return message
 
